15af1f193aa73a8b8517a775c7023cb04ec388b0
Harsh <hs5686584@gmail.com>
2025-07-11 23:01:16 +0530

    workout grid part done

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,7 +1,48 @@
+import { workoutProgram as training_plan } from "../utils/index.js";
+import WorkoutCard from "./WorkoutCard.jsx";
+
 import React from "react";
 
 function Grid() {
-  return <div>Grid</div>;
+  const isLocked = false;
+  const selectedWorkout = 0;
+  return (
+    <div className="training-grid-plan">
+      {Object.keys(training_plan).map((item, index) => {
+        const type =
+          index % 3 === 0 ? "Push" : index % 3 === 1 ? "Pull" : "Legs";
+
+        const trainingPlan = training_plan[index];
+
+        if (index == selectedWorkout) {
+          return <WorkoutCard />;
+        }
+        return (
+          <button key={index}>
+            <div className="plan-card-header">
+              <p>Day {index / 8 <= 1 ? "0" + (index + 1) : index + 1}</p>
+            </div>
+
+            {isLocked ? (
+              <i className="fa-solid fa-lock"></i>
+            ) : index % 3 === 0 ? (
+              <i className="fa-solid fa-dumbbell"></i>
+            ) : index % 3 === 1 ? (
+              <i className="fa-solid fa-weight-hanging"></i>
+            ) : (
+              <i className="fa-solid fa-bolt"></i>
+            )}
+
+            <div className="plan-card-header">
+              <h4>
+                <b>{type}</b>
+              </h4>
+            </div>
+          </button>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Grid;
