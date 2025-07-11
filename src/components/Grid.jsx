ad7853ac0d1f62881403e89248a7598957c1c24c
Harsh <hs5686584@gmail.com>
2025-07-11 23:34:16 +0530

    at 22:21

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function Grid() {
   const isLocked = false;
-  const selectedWorkout = 0;
+  const selectedWorkout = 5;
   return (
     <div className="training-grid-plan">
       {Object.keys(training_plan).map((item, index) => {
@@ -13,25 +13,34 @@ function Grid() {
           index % 3 === 0 ? "Push" : index % 3 === 1 ? "Pull" : "Legs";
 
         const trainingPlan = training_plan[index];
+        const dayNum = index / 8 <= 1 ? "0" + (index + 1) : index + 1;
+        const icon =
+          index % 3 === 0 ? (
+            <i className="fa-solid fa-dumbbell"></i>
+          ) : index % 3 === 1 ? (
+            <i className="fa-solid fa-weight-hanging"></i>
+          ) : (
+            <i className="fa-solid fa-bolt"></i>
+          );
 
         if (index == selectedWorkout) {
-          return <WorkoutCard />;
+          return (
+            <WorkoutCard
+              trainingPlan={trainingPlan}
+              index={index}
+              type={type}
+              icon={icon}
+              dayNum={dayNum}
+            />
+          );
         }
         return (
           <button key={index}>
             <div className="plan-card-header">
-              <p>Day {index / 8 <= 1 ? "0" + (index + 1) : index + 1}</p>
+              <p>Day {dayNum}</p>
             </div>
 
-            {isLocked ? (
-              <i className="fa-solid fa-lock"></i>
-            ) : index % 3 === 0 ? (
-              <i className="fa-solid fa-dumbbell"></i>
-            ) : index % 3 === 1 ? (
-              <i className="fa-solid fa-weight-hanging"></i>
-            ) : (
-              <i className="fa-solid fa-bolt"></i>
-            )}
+            {isLocked ? <i className="fa-solid fa-lock"></i> : icon}
 
             <div className="plan-card-header">
               <h4>
--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-function WorkoutCard() {
-  return <div>WorkoutCard</div>;
+function WorkoutCard({ trainingPlan, index, type, icon, dayNum }) {
+  const { warmup, workout } = trainingPlan || {};
+
+  return (
+    <div className="workout-container">
+      <div className="workout-card card">
+        <div className="plan-card-header">
+          <p>Day {dayNum}</p>
+          {icon}
+        </div>
+        <div className="plan-card-header">
+          <h2>
+            <b>{type} workout</b>
+          </h2>
+        </div>
+      </div>
+
+      <div className="workout-grid">
+        <div className="exercise-name">
+          <h4>Warmup</h4>
+        </div>
+        <h6>Sets</h6>
+        <h6>Reps</h6>
+        <h6 className="weight-input">Max Weights</h6>
+        {warmup.map((warmupExercise, warmupIndex) => {
+          return (
+            <React.Fragment key={warmupIndex}>
+              <div className="exercise-name">
+                <p>
+                  {warmupIndex + 1}. {warmupExercise.name}
+                </p>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default WorkoutCard;
