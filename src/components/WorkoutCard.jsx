c0071d7296e6d943ee76d7b92f08717b391debc7
Harsh <hs5686584@gmail.com>
2025-07-12 22:31:17 +0530

    workout grid completed

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -32,6 +32,70 @@ function WorkoutCard({ trainingPlan, index, type, icon, dayNum }) {
                   {warmupIndex + 1}. {warmupExercise.name}
                 </p>
               </div>
+              <p className="exercise-info">{warmupExercise.sets}</p>
+              <p className="exercise-info">{warmupExercise.reps}</p>
+              <input placeholder="N/A" disabled className="weight-input" />
+            </React.Fragment>
+          );
+        })}
+      </div>
+
+      <div className="workout-card card">
+        <div className="plan-card-header">
+          <p>Day {dayNum}</p>
+          {icon}
+        </div>
+        <div className="plan-card-header">
+          <h2>
+            <b>{type} workout</b>
+          </h2>
+        </div>
+      </div>
+
+      <div className="workout-grid">
+        <div className="exercise-name">
+          <h4>Warmup</h4>
+        </div>
+        <h6>Sets</h6>
+        <h6>Reps</h6>
+        <h6 className="weight-input">Max Weights</h6>
+        {warmup.map((warmupExercise, warmupIndex) => {
+          return (
+            <React.Fragment key={warmupIndex}>
+              <div className="exercise-name">
+                <p>
+                  {warmupIndex + 1}. {warmupExercise.name}
+                </p>
+                <button className="help-icon">
+                  <i className="fa-regular fa-circle-question" />
+                </button>
+              </div>
+              <p className="exercise-info">{warmupExercise.sets}</p>
+              <p className="exercise-info">{warmupExercise.reps}</p>
+              <input placeholder="N/A" disabled className="weight-input" />
+            </React.Fragment>
+          );
+        })}
+      </div>
+
+      <div className="workout-grid">
+        <div className="exercise-name">
+          <h4>Workout</h4>
+        </div>
+        <h6>Sets</h6>
+        <h6>Reps</h6>
+        <h6 className="weight-input">Max Weights</h6>
+        {workout.map((workoutExercise, workoutIndex) => {
+          return (
+            <React.Fragment key={workoutIndex}>
+              <div className="exercise-name">
+                <p>
+                  {workoutIndex + 1}. {workoutExercise.name}
+                </p>
+              </div>
+              <p className="exercise-info">{workoutExercise.sets}</p>
+              <p className="exercise-info">{workoutExercise.reps}</p>
+              <input placeholder="N/A" disabled className="weight-input" />
             </React.Fragment>
           );
         })}
